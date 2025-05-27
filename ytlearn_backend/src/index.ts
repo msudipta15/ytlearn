0402b2d5599a3a0886a8ea8ca3771426d072fc63
Sudipta <techtv70@gmail.com>
2025-05-28 03:01:09 +0530

    setup yt api

--- ytlearn_backend/src/index.ts
+++ ytlearn_backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import axios from "axios";
 
 dotenv.config();
 
@@ -21,5 +22,25 @@ async function main() {
 }
 
 main();
+
+async function getvideodetails() {
+  if (!process.env.YT_API_CODE) {
+    console.log("Invalid youtube Api key");
+    return;
+  }
+  let link =
+    "https://www.youtube.com/watch?v=iVCzmDwIQpA&t=939s&ab_channel=ChaiaurCode";
+
+  const api_key: String = process.env.YT_API_CODE;
+  const videoid = link.split("=")[1];
+
+  const response =
+    await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=YOUR_API_KEY
+     &part=snippet,statistics&fields=items(id,snippet,statistics)`);
+}
+
+app.post("/api/v1/addvideo", async function (req, res) {
+  const link = req.body.link;
+});
 
 app.listen(3000);
