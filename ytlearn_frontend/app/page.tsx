143e6fb51664ad5dd95157c57f43ae1160045817
Sudipta <techtv70@gmail.com>
2025-05-29 01:05:25 +0530

    frontend initied with nextjs

---
+++ ytlearn_frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-red-700 font-bold text-5xl">Welcome to YT Learn</h1>
+    </div>
+  );
+}
