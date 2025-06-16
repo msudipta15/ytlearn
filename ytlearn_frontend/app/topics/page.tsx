a19a9e0f2dd15721514d64df2eeadaf955523fd5
Sudipta <techtv70@gmail.com>
2025-06-16 14:48:02 +0530

    .

--- ytlearn_frontend/app/topics/page.tsx
+++ ytlearn_frontend/app/topics/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Sidebar } from "@/components/user/sidebar";
 import { TopicList } from "@/components/user/topiclist";
+import { IoSearchSharp } from "react-icons/io5";
 import axios from "axios";
 
 export default function ManageContent() {
@@ -14,7 +17,24 @@ export default function ManageContent() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="p-8 w-full  ">
+      <div className="p-8 w-full gap-10 flex flex-col  ">
+        <div className=" w-full mt-5 flex justify-center ">
+          <div className="w-1/3 flex  gap-1 justify-center items-center">
+            <Input
+              placeholder="Enter topic name eg: HTML, CSS"
+              className="px-4 py-5 rounded-lg shadow-xl"
+            />
+            <Button
+              size={"lg"}
+              variant={"ghost"}
+              className="px-4 py-4 cursor-pointer"
+            >
+              <IoSearchSharp />
+              Search
+            </Button>
+          </div>
+        </div>
+
         <TopicList />
       </div>
     </div>
