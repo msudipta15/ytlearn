050f9776b51d83384e0fbb09d964848c4f13a30c
Sudipta <techtv70@gmail.com>
2025-06-27 18:41:29 +0530

    .

--- ytlearn_frontend/app/topics/[topic]/page.tsx
+++ ytlearn_frontend/app/topics/[topic]/page.tsx
@@ -1,16 +1,22 @@
 import { Sidebar } from "@/components/user/sidebar";
 import { VideoPageContent } from "@/components/user/videocontent";
 import { VideoPageTopCard } from "@/components/user/videopagetopcard";
+import { type NextPage } from "next";
 
-export default function TopicPage({ params }: { params: { topic: string } }) {
+interface TopicPageProps {
+  params: { topic: string };
+}
+
+const TopicPage: NextPage<TopicPageProps> = ({ params }) => {
   const { topic } = params;
 
   return (
     <div>
       <Sidebar />
-
       <VideoPageTopCard topic={topic} />
       <VideoPageContent topic={topic} />
     </div>
   );
-}
+};
+
+export default TopicPage;
