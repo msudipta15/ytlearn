9beb3c5b7d925d1655867b9ca5234e8317162a06
Sudipta <techtv70@gmail.com>
2025-06-27 16:52:57 +0530

    .

--- ytlearn_frontend/app/topics/[topic]/page.tsx
+++ ytlearn_frontend/app/topics/[topic]/page.tsx
@@ -1,13 +1,12 @@
 import { Sidebar } from "@/components/user/sidebar";
 import { VideoPageContent } from "@/components/user/videocontent";
-import { VideoList } from "@/components/user/videolist";
 import { VideoPageTopCard } from "@/components/user/videopagetopcard";
 
-interface Props {
+type Props = {
   params: {
     topic: string;
   };
-}
+};
 
 export default async function TopicPage({ params }: Props) {
   const { topic } = params;
